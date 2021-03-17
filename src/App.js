9bba7547376d936a6e2c18251d39139dc370f684
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-17 15:46:27 -0400

    added navbar,need to hookup to container

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import GroupsIndexContainer from "./containers/GroupsIndexContainer";
 import GroupFormContainer from "./containers/GroupFormContainer";
-
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <nav className="text-center bg-blue-900 text-white-100 p-4">
-        <NavLink className="inLine-block px-4 py-2" to="/">Groups</NavLink>
-      <NavLink className="inLine-block px-4 py-2" to="/groups/new"> New Group</NavLink>
-      </nav>
+        <nav className="text-center bg-blue-900 text-yellow-100 p-4">
+          <NavLink
+            className="inline-block px-4 py-2"
+            activeClassName="text-yellow-300"
+            exact
+            to="/"
+          >
+            Groups
+          </NavLink>
+          <NavLink
+            className="inline-block px-4 py-2"
+            activeClassName="text-yellow-300"
+            exact
+            to="/groups/new"
+          >
+            New Group
+          </NavLink>
+        </nav>
         <Switch>
           <Route exact path="/">
-            Groups
-    </Route>
-          <Route path="/groups/new">New Group</Route>
+            <GroupsIndexContainer />
+          </Route>
+          <Route path="/groups/new">
+            <GroupFormContainer />
+            </Route>
         </Switch>
       </Router>
     </div>
