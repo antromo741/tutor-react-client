4694ea1afaac3680124e2ca982d5ad5b2710200b
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-17 16:58:35 -0400

    added to groups index to simulate loading with component did mount, also trying to setup the groupslist and groups list item components

--- src/containers/GroupsIndexContainer.js
+++ src/containers/GroupsIndexContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import GroupsList from "../components/GroupsList";
 
 export default class GroupsIndexContainer extends Component {
     state = {
@@ -6,13 +7,25 @@ export default class GroupsIndexContainer extends Component {
         loading: true,
     };
 
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                groups: [
+                    { name: "Calc 1" },
+                    { name: "Physics 1" },
+                ],
+                loading: false,
+            });
+        }, 1000);
+    }
+
     render() {
         return (
             <section className="max-w-6xl mx-auto mt-16">
                 {this.state.loading ? (
                     "loading spinner"
                 ) : (
-                    '<GroupsList groups={this.state.groups} />'
+                    <GroupsList groups={this.state.groups} />
                 )}
             </section>
         );
