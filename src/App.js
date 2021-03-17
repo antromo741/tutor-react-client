5f88880739d9544856589ceebc937ecd8ecdc0ff
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-17 15:40:36 -0400

    trying to add navbar and some links

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <nav className="text-center bg-blue-900 text-white-100 p-4">
+        <NavLink className="inLine-block px-4 py-2" to="/">Groups</NavLink>
+      <NavLink className="inLine-block px-4 py-2" to="/groups/new"> New Group</NavLink>
+      </nav>
         <Switch>
           <Route exact path="/">
             Groups
