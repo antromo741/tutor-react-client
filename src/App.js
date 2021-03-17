b15bc142324bbace42204c0d2b7490415f92dbfd
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-17 15:34:22 -0400

    added group components and containers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import GroupsIndexContainer from "./containers/GroupsIndexContainer";
+import GroupFormContainer from "./containers/GroupFormContainer";
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            Groups
+    </Route>
+          <Route path="/groups/new">New Group</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
