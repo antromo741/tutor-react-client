e11104e03d8acd5ee3c35510bc3397fb919cd408
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-18 16:34:07 -0400

    added component did mountto group show

--- src/containers/GroupShowContainer.js
+++ src/containers/GroupShowContainer.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
 export default class GroupShowContainer extends Component {
+
+    componentDidMount() {
+        const groupId = this.props.match.params.groupId
+        fetch(`http://localhost:3001/groups/${groupId}`)
+            .then(res => res.json())
+            .then(groupJson => {
+                console.log(groupJson);
+            })
+    }
+
+    
     render() {
         return <section>GroupShowContainer</section>
     }
