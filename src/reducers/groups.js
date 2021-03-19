4629ace5b3cb6019e221b4a8c5fc8552bf30c525
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-19 16:34:21 -0400

    in the groups reducer added 2 of the actions,need to do other 2

--- src/reducers/groups.js
+++ src/reducers/groups.js
@@ -1,8 +1,25 @@
+import {
+    START_LOADING_GROUPS,
+    SUCCESSFULLY_LOADED_GROUPS,
+    FAILED_LOADING_GROUPS,
+    ADD_GROUP,
+} from '../actions'
+
 const initialState = {
     loadingState: 'notStarted',
     list: [],
 }
 
-export default function GroupsReducer(state = initialState, action) {
-    return state
+export default function groupsReducer(state = initialState, action) {
+    switch (action.type) {
+        case START_LOADING_GROUPS:
+            return { ...state, loadingState: 'inProgress' }
+        case SUCCESSFULLY_LOADED_GROUPS:
+            return {
+                list: action.payload,
+                loadingState: 'successful',
+            }
+        default:
+            return state
+    }
 }
