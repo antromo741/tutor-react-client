dec4c603811e286f6b9af9e3e137905e2afed01a
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-07-18 13:19:00 -0400

    added a title to the navbar and started adding custom css to get better at adding my own

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import "tailwindcss/tailwind.css";
+import "../App.css";
 
 class Navbar extends Component {
     
 render() {
     return (
+           
+            
             <nav className="text-center bg-blue-900 text-yellow-100 p-4">
+            <div className="headTitle">Tutorfy</div>
                 <NavLink
                     className="inline-block px-4 py-2"
                     activeClassName="text-yellow-300"
@@ -24,6 +28,7 @@ render() {
                     New Subject
                 </NavLink>
             </nav>
+            
         );
     }
 }
